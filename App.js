ae1b49b166f94e922b304c8810ae3819c7ef09d1
Diego T. Fialho <diego.tfialho@gmail.com>
2016-08-17 17:05:44 -0300

    Class 3

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
 	render() {
-		return <h1>Hello Guys</h1>
+		return (
+			<div>
+				<h1>Hello world</h1>
+				<b>Bold</b>
+			</div>
+		)
 	}
 }
-
-// const App = () => <h1>Hello Guys</h1>
 
 export default App
