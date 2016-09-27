7660109b657296836273629f6b822438b1a4efce
Diego T. Fialho <diego.tfialho@gmail.com>
2016-09-27 17:11:29 -0300

    Class 12 added

--- class-11/App.js
+++ class-11/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 class App extends React.Component {
 	constructor(){
 		super();
-		this.state = { val: 0 };
 		this.update = this.update.bind(this);
 		this.state = { increasing: false };
 	}
